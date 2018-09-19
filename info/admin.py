c495eaf4da1b25ce4e68bc237a28860f6ac7ffea
T-101 <t-101@t-101.org>
2018-09-19 20:17:12 +0300

    added inlines for peripheral infos

--- info/admin.py
+++ info/admin.py
@@ -3,8 +3,34 @@ from django.contrib import admin
 from info.models import PersonalInfo, PersonalEmail, PersonalPhoneNumber, ExternalLink, NickName
 
 
+class PhoneNumberInline(admin.TabularInline):
+    model = PersonalPhoneNumber
+    can_delete = True
+
+
+class EmailInline(admin.TabularInline):
+    model = PersonalEmail
+    can_delete = True
+
+
+class LinkInline(admin.TabularInline):
+    model = ExternalLink
+    can_delete = True
+
+
+class NickNameInline(admin.TabularInline):
+    model = NickName
+    can_delete = True
+
+
 class PersonalInfoAdmin(admin.ModelAdmin):
-    list_display = ('real_name',)
+    list_display = ('real_name', 'get_nicks')
+    inlines = (PhoneNumberInline, EmailInline, LinkInline, NickNameInline)
+
+    def get_nicks(self, obj):
+        return ', '.join([x.name for x in obj.nick_names.all()])
+    get_nicks.short_description = 'Nick names'
+    get_nicks.admin_order_field = 'nick_names__name'
 
 
 admin.site.register(PersonalInfo, PersonalInfoAdmin)
