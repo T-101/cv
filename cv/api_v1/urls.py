9b9938710c8fcb7216cc68f38edf817d558552d9
T-101 <t-101@t-101.org>
2018-09-24 16:50:41 +0300

    fixed consistency in API urls. added base_name for main router to differentiate url formation

--- cv/api_v1/urls.py
+++ cv/api_v1/urls.py
@@ -4,10 +4,10 @@ from rest_framework import routers
 from cv.api_v1.views import EmployerViewSet, EmploymentViewSet, EmploymentTaskViewSet, PierViewSet
 
 router = routers.DefaultRouter()
-router.register('employer', EmployerViewSet)
+router.register('employers', EmployerViewSet)
 router.register('employments', EmploymentViewSet)
 router.register('employment_tasks', EmploymentTaskViewSet)
-router.register('pier', PierViewSet)
+router.register('pier', PierViewSet, base_name='pier')
 
 urlpatterns = [
     path('', include(router.urls)),
