9f176d3fdf2975f11e0594954be8a67afc5f9952
T-101 <t-101@t-101.org>
2018-10-28 21:49:15 +0200

    url config to serve react via django

--- config/urls.py
+++ config/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/info/', include('info.api_v1.urls')),
     path('api/v1/cv/', include('cv.api_v1.urls')),
-    path('api/v1/hobby/', include('hobby.api_v1.urls'))
+    path('api/v1/hobby/', include('hobby.api_v1.urls')),
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
