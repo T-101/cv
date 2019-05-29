22a99c714452f0929670097405ec17cc65154a89
Antti Rummukainen <t-101@t-101.org>
2019-05-29 12:19:13 +0300

    use default router only in debug mode, fallback to simple router

--- cv/api_v1/urls.py
+++ cv/api_v1/urls.py
@@ -1,9 +1,10 @@
+from django.conf import settings
 from django.urls import path, include
 from rest_framework import routers
 
 from cv.api_v1.views import EmployerViewSet, EmploymentViewSet, EmploymentTaskViewSet, PierViewSet
 
-router = routers.DefaultRouter()
+router = routers.DefaultRouter() if settings.DEBUG else routers.SimpleRouter()
 router.register('employers', EmployerViewSet)
 router.register('employments', EmploymentViewSet)
 router.register('employment_tasks', EmploymentTaskViewSet)
