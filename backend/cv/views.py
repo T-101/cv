72741805a0f594822d69ab7bef05840854a3ab60
T-101 <antti.rummukainen@gmail.com>
2025-01-23 23:51:21 +0200

    tags to context

--- backend/cv/views.py
+++ backend/cv/views.py
@@ -1,6 +1,7 @@
+from django.db.models.functions import Lower
 from django.views.generic import TemplateView
 
-from cv.models import PersonalInfo
+from cv.models import PersonalInfo, PortfolioItemTag
 
 
 class LandingPageView(TemplateView):
@@ -9,4 +10,5 @@ class LandingPageView(TemplateView):
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
         ctx["info"] = PersonalInfo.load()
+        ctx["tags"] = PortfolioItemTag.objects.order_by(Lower("tag"))
         return ctx
