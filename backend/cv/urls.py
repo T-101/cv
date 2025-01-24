bd32b28b977cf6fcd126929ac164eddf83572808
T-101 <antti.rummukainen@gmail.com>
2025-01-24 17:44:42 +0200

    portfolio item urls

--- backend/cv/urls.py
+++ backend/cv/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from cv.views import LandingPageView
+from cv.views import LandingPageView, PortfolioItemView
 
 app_name = 'cv'
 
 urlpatterns = [
     path('', LandingPageView.as_view(), name='landing_page'),
+    path('item/<slug:slug>/', PortfolioItemView.as_view(), name='portfolio_item'),
 ]
