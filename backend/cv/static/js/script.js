4b15692d49cfe8c6843ff5633d33e19194e3129a
T-101 <antti.rummukainen@gmail.com>
2025-01-24 16:17:11 +0200

    static files

---
+++ backend/cv/static/js/script.js
@@ -0,0 +1,15 @@
+(function ($) {
+
+    "use strict";
+
+    $(document).ready(function () {
+
+        // Animate on Scroll
+        AOS.init({
+            duration: 600,
+            once: true,
+        })
+
+    });
+
+})(jQuery);
